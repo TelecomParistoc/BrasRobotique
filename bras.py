69c2155652b35ed40484e5ff5d3bdaac1dc081f5
superpingu <arnaudbonetti@me.com>
2018-05-11 14:58:34 +0200

    Fix: moving ax12 on launch

--- bras.py
+++ bras.py
@@ -1,9 +1,22 @@
 from AX12 import AX12
 
+def stop(ax12):
+    stopped = False
+    while not stopped:
+       try:
+          ax12.turn(0)
+          stopped = True
+       except:
+          pass
+
 base = AX12(121)
+stop(base)
 epaule = AX12(142)
+stop(epaule)
 coude = AX12(25)
+stop(coude)
 pince = AX12(145)
+stop(pince)
 
 try:
     pince.move(0)
