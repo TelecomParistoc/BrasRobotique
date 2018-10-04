f21ab4d03e0a9dd5e042a041b30fbad3a8f5b410
Julien Benatar <julien.benatar@telecom-paristech.fr>
2018-10-04 23:05:10 +0200

    fonction

--- bras.py
+++ bras.py
@@ -1,4 +1,5 @@
 from AX12 import AX12
+import math
 
 def stop(ax12):
     stopped = False
@@ -22,3 +23,10 @@ try:
     pince.move(-45)
 except:
     pass
+
+def deplacerBras(x, y)
+
+    q2 = acos((x*x+y*y-(l1*l1+l2*l2))/(2*l1*l2));
+	q1 = atan((y*(l1+l2*cos(q2))-x*l2*sin(q2))/(x*(l1+l2*cos(q2))+y*l2*sin(q2)));
+    q2 *= (180/PI);
+    q1 *= (180/PI)+45;
